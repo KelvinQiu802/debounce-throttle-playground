514c4e8d72fd1e759ca9986de82df631493a8644
KelvinQiu <kelvinqiu802@outlook.com>
2022-11-03 14:28:03 +0800

    feat: Counter component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Counter from './components/counter';
+
+function App() {
+  return (
+    <div className='App'>
+      <Counter />
+      <hr />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import debounce from '../utils/debounce';
+
+function Counter() {
+  const [debounceCount, setDebounceCount] = React.useState(0);
+  const [noramlCount, setNormalCount] = React.useState(0);
+
+  const handleDebounceClick = debounce(() => {
+    setDebounceCount((prev) => prev + 1);
+  }, 500);
+
+  return (
+    <div className='counter-group'>
+      <div className='counter'>
+        <button onClick={() => setNormalCount((prev) => prev + 1)}>
+          Normal Click
+        </button>
+        <h1>{noramlCount}</h1>
+      </div>
+      <div className='counter'>
+        <button onClick={handleDebounceClick}>Debounce Click (500ms)</button>
+        <h1>{debounceCount}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
